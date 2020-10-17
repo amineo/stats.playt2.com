03dba5631e4778711a888a2ad386c0c638b049ca
Anthony Mineo <anthonymineo@gmail.com>
2020-10-17 10:50:27 -0400

    Fix meta desc for other gametypes

--- src/pages/public/GameDetail.tsx
+++ src/pages/public/GameDetail.tsx
@@ -33,15 +33,24 @@ const GameDetail = () => {
 					<Helmet>
 						<title>{gameQuery.data.map} [{gameQuery.data.gametype}] - {gameQuery.data.datestamp.split(/[T]/)[0]} | Tribes 2 Stats Project</title>
 						<link rel="canonical" href={`https://stats.playt2.com/game/${gameQuery.data.gameId}`}></link>
-						<meta name="description" content={`Storm(${gameQuery.data.teams.storm.players.length}) ${gameQuery.data.teams.storm.score} -vs- Inferno(${gameQuery.data.teams.inferno.players.length}) ${gameQuery.data.teams.inferno.score}`}/>
-
 						<meta property="og:site_name" content="Tribes 2 Stats Project" />
 						<meta property="og:url" content={`https://stats.playt2.com/game/${gameQuery.data.gameId}`} />
 						<meta property="og:type" content="article" />
 						<meta property="og:title" content={`${gameQuery.data.map} [${gameQuery.data.gametype}] - ${gameQuery.data.datestamp.split(/[T]/)[0]} | Tribes 2 Stats Project`} />
-						<meta property="og:description" content={`Storm(${gameQuery.data.teams.storm.players.length}) ${gameQuery.data.teams.storm.score} -vs- Inferno(${gameQuery.data.teams.inferno.players.length}) ${gameQuery.data.teams.inferno.score}`} />
 						<meta property="og:image" content={`https://stats.playt2.com/logo512.png`} /> 
 					</Helmet>
+
+					{gameQuery.data.gametype === 'CTFGame' || gameQuery.data.gametype === 'SCtFGame' ? (
+						<Helmet>
+							<meta name="description" content={`Storm(${gameQuery.data.teams.storm.players.length}) ${gameQuery.data.teams.storm.score} -vs- Inferno(${gameQuery.data.teams.inferno.players.length}) ${gameQuery.data.teams.inferno.score}`}/>
+							<meta property="og:description" content={`Storm(${gameQuery.data.teams.storm.players.length}) ${gameQuery.data.teams.storm.score} -vs- Inferno(${gameQuery.data.teams.inferno.players.length}) ${gameQuery.data.teams.inferno.score}`} />
+						</Helmet>
+					) : (
+						<Helmet>
+							<meta name="description" content={`${gameQuery.data.players.length} players`}/>
+							<meta property="og:description" content={`${gameQuery.data.players.length} players`} />
+						</Helmet>
+					)}
 					
 					{gameQuery.data.gametype === 'CTFGame' || gameQuery.data.gametype === 'SCtFGame' ? (
 						<CtfGameCard {...gameQuery.data} />
